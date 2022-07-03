e7f18fbf3361c41de24116201da1eee87f0c3a0d
Andy Warr <andywarr@gmail.com>
2022-07-03 15:18:15 -0700

    Added drawUniform

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ import rough from 'roughjs';
 const canvas = document.getElementById("canvas");
 const rc = rough.canvas(canvas);
 
+let circleDiameter = 8;
+let circleSpacing = 2;
+
+let x = 0;
+let y = 0;
+
+function drawUniform() {
+  for (y = circleSpacing + circleDiameter/2; y < canvas.height; y += circleSpacing + circleDiameter) {
+    for (x = circleSpacing + circleDiameter/2; x < canvas.width; x += circleSpacing + circleDiameter) {
+      rc.circle(x, y, circleDiameter);
+    }
+  }
+}
+
 function setSize(height, width) {
   canvas.height = height;
   canvas.width = width;
@@ -17,6 +31,8 @@ function windowResize() {
 function init() {
   setSize(window.innerHeight, window.innerWidth);
   window.onresize = windowResize;
+
+  drawUniform();
 }
 
 init();
