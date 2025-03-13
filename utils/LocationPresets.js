d7fa237b8688d4f89ceaddead7bdd1035cf4cac4
Andy Warr <andywarr@gmail.com>
2025-03-12 22:42:55 -0700

    Add LocationPresets utility file

---
+++ utils/LocationPresets.js
@@ -0,0 +1,18 @@
+export const LocationPresets = {
+  everest: {
+    center: { lat: 27.9881198, lng: 86.8425776 },
+    zoom: 10,
+  },
+  nyc: {
+    center: { lat: 40.6966727, lng: -74.1443534 },
+    zoom: 12,
+  },
+  sanFrancisco: {
+    center: { lat: 37.7709704, lng: -122.4118542 },
+    zoom: 12,
+  },
+  tahoe: {
+    center: { lat: 39.088311, lng: -120.013428 },
+    zoom: 10,
+  },
+};
